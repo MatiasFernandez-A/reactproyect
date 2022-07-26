7224761fce6100188134ffeb4ad8e78789d3e66b
Matias Fernandez Almeda <fernandezalmedamatias@gmail.com>
2022-07-26 18:03:39 -0300

    Agregando NavBar con estilos

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import "./NavBar.css"; 
+
+function NavBar () {
+    return (
+        <header>
+            <nav>
+                <div className="title_container">
+                    <h1 className="title"><a href="#">Todo maderas</a></h1>    
+                </div>
+                <ul className="list">
+                    <li><a href="#" className="ancore_list">Inicio</a></li>
+                    <li><a href="#" className="ancore_list">nosotros</a></li>
+                    <li><a href="#" className="ancore_list">Productos</a></li>
+                    <li><a href="#" className="ancore_list">Contactos</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default NavBar; 
