284fe364ca9139b302769a1ab00871c055562c63
Matias Fernandez Almeda <fernandezalmedamatias@gmail.com>
2022-08-03 22:36:19 -0300

    Agregando contador de productos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import ItemCount from './components/ItemCount/ItemCount';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 
-function App() {
+function App() {  
   return (
     <div>
       <NavBar />
       <ItemListContainer greeting="Se vende Todo madera"/>
+      <ItemCount initial={1} stock={10}/>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./ItemCount.css";
+
+function ItemCount (props){
+    const [clicks, setClicks] = React.useState(1);
+
+    function handleIncrement () {
+        if (clicks < props.stock){
+            setClicks (clicks + 1);
+        } else {
+            alert("Ya no hay mas stock")
+        }
+    }
+    function handleDecrement () {
+        if (clicks > props.initial){
+            setClicks (clicks - 1);
+        } else {
+            alert("Si o si tenes que llevar comprar al menos un producto")
+        }
+    }
+
+    return(
+        <div className='counter-container'>
+            <h3>Contador</h3>
+            <div className='buttons-container'>
+                <button onClick={handleDecrement}> - </button>
+                <h3>{clicks}</h3>
+                <button onClick={handleIncrement}> + </button>
+            </div>
+            <div className='cartButtonContainer'>
+                <button>Agregar al carrito</button>
+            </div>
+        </div>
+    )
+}
+export default ItemCount;
