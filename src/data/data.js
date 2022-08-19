847a3b4497b6eb98228660fae984616ed1d06d8d
Matias Fernandez Almeda <fernandezalmedamatias@gmail.com>
2022-08-18 21:32:26 -0300

    Agregando img product

--- src/data/data.js
+++ src/data/data.js
@@ -11,7 +11,7 @@ const itemsData = [{
     "model": "Huett",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 3847,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw15a8f491/images/D-15M/D-15M_f.jpg?sw=600&sh=600&sm=fit",
     "category": "B",
     "stock": 10
   }, {
@@ -19,7 +19,7 @@ const itemsData = [{
     "model": "Youel",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 4136,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw0c0f5e76/images/000-15M/000-15M_f.jpg?sw=600&sh=600&sm=fit",
     "category": "A",
     "stock": 4
   }, {
@@ -27,7 +27,7 @@ const itemsData = [{
     "model": "Krugmann",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 4770,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw1d09606f/images/000-15M-StreetMaster/000-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
     "category": "B",
     "stock": 4
   }, {
@@ -35,7 +35,7 @@ const itemsData = [{
     "model": "Wheelband",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 5399,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dwfca56267/images/00-15M/00-15M_f.jpg?sw=600&sh=600&sm=fit",
     "category": "A",
     "stock": 4
   }, {
@@ -43,7 +43,7 @@ const itemsData = [{
     "model": "MacShirrie",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 9463,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=1600&sh=1600&sm=fit",
     "category": "B",
     "stock": 9
   }, {
@@ -51,7 +51,7 @@ const itemsData = [{
     "model": "Streak",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 9762,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dweab99598/images/00-28-Modern-Deluxe/00-28-Modern-Deluxe_f.jpg?sw=1600&sh=1600&sm=fit",
     "category": "A",
     "stock": 9
   }, {
@@ -59,7 +59,7 @@ const itemsData = [{
     "model": "Knowlson",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 4856,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6c32acd4/images/D-42-Modern-Deluxe/D-42-Modern-Deluxe_f.jpg?sw=1600&sh=1600&sm=fit",
     "category": "B",
     "stock": 10
   }, {
@@ -67,7 +67,7 @@ const itemsData = [{
     "model": "Sealy",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 3447,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw21b5a317/images/000-18-Modern-Deluxe/000-18-Modern-Deluxe_f.jpg?sw=1600&sh=1600&sm=fit",
     "category": "A",
     "stock": 4
   }, {
@@ -75,7 +75,7 @@ const itemsData = [{
     "model": "Lutton",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 4384,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw33ef8c3d/images/OMC-X1E/OMC-X1E_f.jpg?sw=1600&sh=1600&sm=fit",
     "category": "B",
     "stock": 10
   }, {
@@ -83,7 +83,7 @@ const itemsData = [{
     "model": "Verty",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 43484,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dwbff7160f/images/D-X2E/D-X2E_f_rosewood.jpg?sw=1600&sh=1600&sm=fit",
     "category": "B",
     "stock": 15
   }, {
@@ -91,7 +91,7 @@ const itemsData = [{
     "model": "Matck",
     "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat",
     "price": 1390,
-    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=600&sh=600&sm=fit",
+    "img": "https://www.martinguitar.com/dw/image/v2/BGJT_PRD/on/demandware.static/-/Sites-martin-master-catalog/default/dw6bf5b973/images/D-15M-StreetMaster/D-15M-StreetMaster_f.jpg?sw=1600&sh=1600&sm=fit",
     "category": "A",
     "stock": 3
   }]
