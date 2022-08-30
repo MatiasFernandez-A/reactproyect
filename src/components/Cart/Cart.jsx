b1b64ff7adac5b09cadf6ea8ade2089a3f7a2b67
Matias Fernandez Almeda <fernandezalmedamatias@gmail.com>
2022-08-30 19:31:57 -0300

    prueba

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {cartContext} from '../../store/cartContext'
 
-function Cart() {
+function Cart( ) {
+
+  let cartView = useContext(cartContext)
+  console.log(`Esto hay dentro del carrito ${cartView}`);
+
   return (
-    <div>Esta es la pagina de carrito</div>
+    <div>
+      <p>Esta es la pagina de carrito</p>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>model</p>
+          <p>Precio</p>
+        </div>
+        <div>
+          <button>Eliminar</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
